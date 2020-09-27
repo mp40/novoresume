54e9849edf020f2b9509a09f4efa01d7188f9880
Matthew Philps <philps.matthew@gmail.com>
2020-09-27 20:25:30 +0900

    fetch tested

--- client/src/fetch/fetch.spec.js
+++ client/src/fetch/fetch.spec.js
@@ -1,7 +1,91 @@
-import { wiringTest } from "./index";
+import { fetchSignup, fetchSignin } from "./index";
 
-describe("Calling the API", () => {
-  it("should pass wring test", () => {
-    expect(wiringTest()).toBe("working");
+describe("Calling the Server", () => {
+  afterEach(() => {
+    global.fetch.mockClear();
+  });
+
+  it("should post new user to /signup", async () => {
+    global.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve(
+        JSON.stringify({
+          _id: "1a",
+          firstName: "John",
+          lastName: "Test",
+          email: "testSan@gmail.com",
+          password: "hashed_password",
+        })
+      );
+    });
+
+    const user = {
+      firstName: "John",
+      lastName: "Test",
+      email: "testSan@gmail.com",
+      password: "password",
+    };
+
+    const res = await fetchSignup(user);
+
+    expect(JSON.parse(res)).toEqual({
+      _id: "1a",
+      firstName: "John",
+      lastName: "Test",
+      email: "testSan@gmail.com",
+      password: "hashed_password",
+    });
+  });
+
+  it("should return error message on post /signup failure", async () => {
+    global.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject();
+    });
+
+    const user = {
+      firstName: "John",
+      lastName: "Test",
+      email: "testSan@gmail.com",
+      password: "password",
+    };
+
+    const res = await fetchSignup(user);
+
+    expect(res).toEqual("Signup Error");
+  });
+
+  it("should post new user to /signin", async () => {
+    global.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve(
+        JSON.stringify({
+          message: "Signed In",
+        })
+      );
+    });
+
+    const user = {
+      email: "testSan@gmail.com",
+      password: "password",
+    };
+
+    const res = await fetchSignin(user);
+
+    expect(JSON.parse(res)).toEqual({
+      message: "Signed In",
+    });
+  });
+
+  it("should return error message on post /signin failure", async () => {
+    global.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject();
+    });
+
+    const user = {
+      email: "testSan@gmail.com",
+      password: "password",
+    };
+
+    const res = await fetchSignin(user);
+
+    expect(res).toEqual("Signin Error");
   });
 });
--- client/src/fetch/index.js
+++ client/src/fetch/index.js
@@ -1,3 +1,39 @@
-export const wiringTest = () => {
-  return "working";
+export const fetchSignup = async (user) => {
+  let res;
+
+  await fetch("/signup", {
+    method: "post",
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response;
+    })
+    .then((data) => {
+      res = data;
+    })
+    .catch((error) => {
+      res = "Signup Error";
+    });
+
+  return res;
+};
+
+export const fetchSignin = async (user) => {
+  let res;
+
+  await fetch("/signin", {
+    method: "post",
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response;
+    })
+    .then((data) => {
+      res = data;
+    })
+    .catch((error) => {
+      res = "Signin Error";
+    });
+
+  return res;
 };
