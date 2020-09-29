7b079a6440843e043527216d15b07f7ac3110a13
Matthew Philps <philps.matthew@gmail.com>
2020-09-29 23:21:44 +0900

    blog banner

---
+++ client/src/components/home/blogBanner/data.js
@@ -0,0 +1,31 @@
+export const text = {
+  heading: "Recent blog posts",
+  subHeading1: "Personal Items",
+  subHeading2: "Personal Items",
+  subHeading3: "Personal Items",
+  body1:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit pellentesque habitant. Fermentum et sollicitudin ac orci phasellus egestas tellus. Elementum sagittis vitae et leo duis ut. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Vulputate mi sit amet mauris commodo. Massa massa ultricies mi quis hendrerit dolor.",
+  body2:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit pellentesque habitant. Fermentum et sollicitudin ac orci phasellus egestas tellus. Elementum sagittis vitae et leo duis ut. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Vulputate mi sit amet mauris commodo. Massa massa ultricies mi quis hendrerit dolor.",
+  body3:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit pellentesque habitant. Fermentum et sollicitudin ac orci phasellus egestas tellus. Elementum sagittis vitae et leo duis ut. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Vulputate mi sit amet mauris commodo. Massa massa ultricies mi quis hendrerit dolor.",
+  link: "Read now ...",
+};
+
+export const blogs = [
+  {
+    icon: "image1",
+    title: text.subHeading1,
+    text: text.body1,
+  },
+  {
+    icon: "image2",
+    title: text.subHeading2,
+    text: text.body2,
+  },
+  {
+    icon: "image3",
+    title: text.subHeading3,
+    text: text.body3,
+  },
+];
--- client/src/components/home/blogBanner/index.jsx
+++ client/src/components/home/blogBanner/index.jsx
@@ -1,12 +1,48 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-import text from "./text";
+import PseudoLogo from "../../pseudoLogo";
+
+import image1 from "../../../assets/background/image1.png";
+import image2 from "../../../assets/background/image2.png";
+import image3 from "../../../assets/background/image3.png";
+
+import { blogs, text } from "./data";
 
 import "./styles.css";
 
+const icons = { image1, image2, image3 };
+
 const HomeBlogBanner = () => {
-  return <div></div>;
+  return (
+    <div className='homeBlogBanner'>
+      <div className='heading'>
+        <PseudoLogo />
+        <h2>{text.heading}</h2>
+      </div>
+      <div className='body'>
+        {blogs.map((blog) => {
+          return (
+            <div className='blogs' key={blog.icon}>
+              <div className='image'>
+                <img src={icons[blog.icon]} alt={blog.icon} />
+              </div>
+              <div className='text'>
+                <h3>{blog.title}</h3>
+                <p>{blog.text}</p>
+                <a
+                  href='https://novoresume.com/career-blog'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  {text.link}
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 HomeBlogBanner.propTypes = {};
